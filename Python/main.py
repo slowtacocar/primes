e13442c8a2b65f1a74950e71484eb1001d78034e
Jeff George <jeffgeorge@Jeffs-MBP13.local>
2021-02-06 17:16:02 -0500

    Adding Python Version

---
+++ Python/main.py
@@ -0,0 +1,28 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+def checkPrime(x, primes):
+    for value in primes:
+        if x % value == 0:
+            return False
+    return True
+
+def checkPrimeSlow(x):
+    for value in range(x-1, 1, -1):
+        if x % value == 0:
+            return False
+    return True
+
+
+values = [2]
+maxNum = 100000  # int(input("num: "))
+for x in range(3, maxNum):
+    if (checkPrime(x, values)):
+    #if (checkPrimeSlow(x)):
+        values.append(x)
+print(values)
+print(len(values))
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
