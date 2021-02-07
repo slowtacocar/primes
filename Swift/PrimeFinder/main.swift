efe172f8969d10d01c0ad54c247562f2448818a2
Jeff George <jefgeo@icloud.com>
2021-02-07 09:56:21 -0500

    Updated to avoid passing values array to check function.  No change to runtime.

--- Swift/PrimeFinder/main.swift
+++ Swift/PrimeFinder/main.swift
@@ -7,8 +7,10 @@
 
 import Foundation
 
-func checkPrime(x: Int, primes: [Int]) -> Bool {
-    for value in primes {
+var values:[Int] = [2]
+
+func checkPrime(x: Int) -> Bool {
+    for value in values {
         if (x % value == 0){
             return false;
         }
@@ -16,16 +18,13 @@ func checkPrime(x: Int, primes: [Int]) -> Bool {
     return true;
 }
 
-var values = [2]
-
-
 //if let value = readLine(), let maxNum = Int(value) {
-
-var maxNum = 100000
+NSLog("Start")
+var maxNum = 200000
     for x in 3...maxNum {
-        if (checkPrime(x: x, primes: values)) {
+        if (checkPrime(x: x)) {
             values.append(x)
         }
         }
     print(values)
-
+NSLog("Finish")
